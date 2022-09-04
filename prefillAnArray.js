7d0dbf7a8c5c3088d5581e3f444b706a6bf940a7
Lokesh Sirna <lokesh_sirna@epam.com>
2022-09-04 15:10:51 +0530

    Modified prefillArray.js

--- prefillAnArray.js
+++ prefillAnArray.js
@@ -1,13 +1,25 @@
 function prefill(n, v) {
-    if (parseInt(n) !== Math.abs(n)) {
+    let value=parseInt(n);
+    if(isNaN(value))
+      {
         throw new TypeError(`${n} is invalid`);
-    }
-    else if (n == 0) {
+      }
+    else if(!Number.isInteger(parseFloat(n)))
+      {
+        throw new TypeError(`${n} is invalid`);
+      }
+    else if(value<0)
+      {
+        throw new TypeError(`${n} is invalid`);
+      }
+    else if(n==0)
+      {
         return [];
-    }
-    else {
-        let array = new Array(n);
+      }
+    else
+      {
+        let array=new Array(n);
         array.fill(v);
         return array;
-    }
-}
+      }
+  }
