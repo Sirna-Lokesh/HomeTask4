34da862736e899c99f4bc651eea858fdf7016325
Lokesh Sirna <lokesh_sirna@epam.com>
2022-09-08 23:38:23 +0530

    Modified prefillArray.js

--- prefillAnArray.js
+++ prefillAnArray.js
@@ -1,25 +1,14 @@
 function prefill(n, v) {
-    let value=parseInt(n);
-    if(isNaN(value))
-      {
-        throw new TypeError(`${n} is invalid`);
-      }
-    else if(!Number.isInteger(parseFloat(n)))
-      {
-        throw new TypeError(`${n} is invalid`);
-      }
-    else if(value<0)
-      {
-        throw new TypeError(`${n} is invalid`);
-      }
-    else if(n==0)
-      {
-        return [];
-      }
-    else
-      {
-        let array=new Array(n);
-        array.fill(v);
-        return array;
-      }
+  let value = parseInt(n);
+  if (isNaN(value) || !Number.isInteger(parseFloat(n)) || value < 0) {
+    throw new TypeError(`${n} is invalid`);
   }
+  else if (n == 0) {
+    return [];
+  }
+  else {
+    let array = new Array(n);
+    array.fill(v);
+    return array;
+  }
+}
